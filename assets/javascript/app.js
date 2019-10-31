aad690813f6ac62ed133f883457b9093f984142d
Ross McLane <ross.mclane@gmail.com>
2019-10-30 20:02:27 -0400

    Adding results functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,134 @@
 // PseudoCode
 
+// To Dos
+// - Make it slightly prettier
+// - Add a readme
+
 // Initialize Variables
-// - time, wins, losses, unanswered
-let gameTime = 60;
-const wins = 0;
-const losses = 0;
-const unanswered = 0;
+let gameTime = 30;
+let wins = 0;
+let losses = 0;
+let unanswered = 0;
 
-// If a button with the start class is in the document
-// set an event listener on that button
-// event Listener - on click, renderGame
-
-// function displayStart
-// Show Start button
-
-// displayStart on load
-function renderStart() {
-    let startButton = $("<button>")
-        // translate these into adding a class
-        .css('height', '100px')
-        .css('width', '200px')
-        .css('font-size', '30px')
-        .text('Start').css('display', 'block')
-        .css('margin', '0 auto').attr('id', 'start-button');
-
-    $("#game").append(startButton);
-
-    $("#start-button").on('click', function () {
-        console.log('Render Game!');
-        renderGame();
-    })
-}
-
+// start the game
 renderStart();
 
+function renderStart() {
 
-// const question1 = $("<div>").text('Hello').css('text-align', 'center');
-// $("#game").append(question1);
+    const startButton = $("<button>")
+        .text('Start')
+        .addClass('buttonClass')
+        .on('click', function () {
+            renderGame();
+        })
+
+    $("#game").append(startButton);
+}
 
 function renderGame() {
-    // Remove the Start Button
 
     $("#game").empty();
 
-    $("#game")
+    // Initialize Timer
+    let timer = $("<div>").text(`You have ${gameTime} Seconds Left`)
+        .css('text-align', 'center')
+        .css('margin-bottom', '50px')
 
-    for (question of questions) {
-        
-        let mcQuestion = $("<div>").addClass('question').css('text-align', 'center').css('margin-bottom','40px');
+    $("#game").append(timer);
+
+    interval = setInterval(function () {
+        if (gameTime === 0) {
+            console.log('End of Game!');
+            // clear interval
+            clearInterval(interval);
+            calculateResults();
+            renderResults();
+        } else {
+            gameTime--;
+            timer.text(`You have ${gameTime} Seconds Left`);
+        }
+
+    }, 1000);
+
+    // For each question, render the question to the DOM
+    for ([questionNumber, question] of questions.entries()) {
+
+        let mcQuestion = $("<div>").addClass('question');
         let prompt = $("<div>").text(question.prompt);
         mcQuestion.append(prompt);
 
         for (let i = 0; i < 4; i++) {
 
             let label = $("<label class='radio-inline'>")
-                .html(`<input type="radio" name="optradio"> ${question.possibleAnswers[i]}`)
-                .css('font-size','14px').css('margin-right','20px');
+                .html(`<input type="radio" name="${questionNumber}" value='${question.possibleAnswers[i]}'> ${question.possibleAnswers[i]}`)
+                .css('font-size', '14px').css('margin-right', '20px');
 
             mcQuestion.append(label)
         }
 
         $('#game').append(mcQuestion);
+    }
+
+    // Render Done Button and attach click listener to render results
+    const doneButton = $("<button>").text('Done').addClass('buttonClass');
+
+    doneButton.on('click', function () {
+        clearInterval(interval);
+        calculateResults();
+        renderResults();
+    })
+
+    $("#game").append(doneButton);
+
+}
+
+function calculateResults() {
+
+    // for each question
+    for (let i = 0; i < questions.length; i++) {
+
+        // get all answer choices
+        let possibleAnswers = $(`[name="${i}"]`);
+
+        // set default answered state to false
+        let answered = false;
+
+        for (answer of possibleAnswers) {
+
+            // if answer is checked
+            if ($(answer).is(':checked')) {
+
+                answered = true;
+
+                if ($(answer).val() === questions[i].correctAnswer) {
+                    wins++
+                } else {
+                    losses++;
+                }
+            }
+        }
+
+        if (!answered) {
+            unanswered++;
+        }
 
     }
 
 }
 
-// Start by displaying the starting time, there is a one second delay
-$("#timer").text(`You have ${gameTime} Seconds Left`);
-interval = setInterval(function () {
-    if (gameTime === 0) {
-        console.log('End of Game!');
-        // clear interval
-        clearInterval(interval);
-        // - renderResults
-    } else {
-        gameTime--;
-        $("#timer").text(`You have ${gameTime} Seconds Left`);
-    }
+function renderResults() {
 
-}, 1000);
+    $("#game").empty();
 
-// Counts down in increments of one second and each time 
-// It finishes, it redisplays that part of the DOM
-// At the end of the timer it ends the game and 
-// Displays renderResults
+    const resultsDiv = $("<div>");
 
-// Initially renderGame
-// Start Timer 
+    const h1Tag = $("<h1 class='title'>").text("All Done!").css('font-size', '30px');
+    const winsDisplay = $("<p>").text(`Wins: ${wins}`).css('text-align', 'center');
+    const lossesDisplay = $("<p>").text(`Losses: ${losses}`).css('text-align', 'center');
+    const unansweredDisplay = $("<p>").text(`Unanswered: ${unanswered}`).css('text-align', 'center');
 
-// function renderForm
-// Displays all questions
+    resultsDiv.append(h1Tag, winsDisplay, lossesDisplay, unansweredDisplay);
 
-// function renderResults
-// Calculates results and renders them to the DOM
+    $("#game").append(resultsDiv);
 
-// Set click listener on Submit
-// - Clears the questions and then displays the results
+}
